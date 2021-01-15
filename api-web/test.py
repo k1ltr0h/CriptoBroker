32af6b6301727dccb87634690f9e6bf063c7c218
christiangfv <christiangfv@gmail.com>
2021-01-15 13:35:49 -0300

    'ActtualizacionInforme'

--- api-web/test.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector  
-  
-#Create the connection object   
-myconn = mysql.connector.connect(host = "192.168.64.2", user = "chris",passwd = "Wertaste11")  
-  
-#printing the connection object   
-print(myconn)  
